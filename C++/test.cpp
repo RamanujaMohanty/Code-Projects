ef1d1c11e9ec8c3826b0661fb02bbee9c7faebc9
RamanujaMohanty <rmohanty@mail.sfsu.edu>
2025-03-11 12:16:55 -0700

    Create test.cpp
    Made test.cpp in C++

---
+++ C++/test.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main(void)
+{
+        std::cout << "Hello World!" << std::endl;
+        std::string s = "Whatever";
+        int len = s.length();
+        char ch = s[3];
+        std::cout << ch << std::endl;
+        std::cout << len << std::endl;
+        return 0;
+}
