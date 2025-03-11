b8223b0d0371c340b111c725b4184c3d6f379e81
RamanujaMohanty <rmohanty@mail.sfsu.edu>
2025-03-11 12:11:56 -0700

    Create stringTester.java
    Made stringTester.java inside testProjects.

---
+++ Java/testProjects/stringTester.java
@@ -0,0 +1,14 @@
+import java.lang.String;
+public class stringTester
+{
+        public static void main(String[] args)
+        {
+                // String a = "This is just some arbitrary string guys!";
+                // int b = a.codePointAt(3);
+                // System.out.println(b);
+                char c = 97;
+                System.out.println(c);
+                char d = c - 32;
+                System.out.println(d);
+        }
+}
