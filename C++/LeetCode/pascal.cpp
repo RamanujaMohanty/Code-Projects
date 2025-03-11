33d655c34da33a0d6ad8e7d0d5cd03f10a04ed43
RamanujaMohanty <rmohanty@mail.sfsu.edu>
2025-03-11 12:22:14 -0700

    Create pascal.cpp
    Made pascal.cpp

---
+++ C++/LeetCode/pascal.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int rows;
+    cout << "Enter the number of rows for Pascal's Triangle: ";
+    cin >> rows;
+
+    for (int i = 0; i < rows; i++) {
+        int coef = 1;
+        for (int j = 0; j <= i; j++) {
+            cout << coef << " ";
+            coef = coef * (i - j) / (j + 1);
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
