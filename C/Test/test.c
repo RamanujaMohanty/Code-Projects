dcac8f1992a80be833a8d46e79f031d0d7ceb6ce
RamanujaMohanty <rmohanty@mail.sfsu.edu>
2025-03-12 15:55:48 -0700

    Create test.c
    Made Test directory & test.c

---
+++ C/Test/test.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main()
+{
+    /*
+    int i = 2, j = 3;
+    int m, n;
+    m = i++;
+    n = ++j;
+    printf("m = %d \t i = %d \n", m, i);
+    printf("m = %d \t i = %d \n", n, j);
+    return 0;
+    */
+    int num = 10;
+    int k = (10<<3)&1;
+    printf("%d\n", k);
+    return 0;
+}
