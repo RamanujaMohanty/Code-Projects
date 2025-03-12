f5e56921f3e0d2a4a6a861c782d7059fa96ffdb9
RamanujaMohanty <rmohanty@mail.sfsu.edu>
2025-03-12 15:57:08 -0700

    Added test.c(1-4)
    Added files: test1.c, test2.c, test3.c, test4.c.

---
+++ C/Test/test3.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main()
+{
+    for (int ii = 0; ii < 3; ii = ii + 1)
+        printf("a\n");
+        printf("b\n");
+    printf("c\n");
+}
---
+++ C/Test/test4.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int a = 0;
+    for (int ii = 0; ii < 5; ii++)
+    {
+        a++;
+    }
+    printf("%d",a);
+    return 0;
+}
