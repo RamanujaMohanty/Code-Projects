7e025e762a892cca1605b768b238e2b09ae8dafa
Ramanuja Mohanty <rmohanty@mail.sfsu.edu>
2025-06-25 20:22:59 -0700

    Added contents of test3.c to file.

--- C/Test/for_loop.c
+++ C/Test/for_loop.c
@@ -8,5 +8,11 @@ int main(void)
         a++;
     }
     printf("%d\n",a);
+
+    for (int ii = 0; ii < 3; ii = ii + 1)
+        printf("a\n");
+        printf("b\n");
+    printf("c\n");
+    
     return 0;
 }
