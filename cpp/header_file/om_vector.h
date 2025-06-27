09d4d5b3b6684277d6829b2c4a72d38f9f5bc263
Ramanuja Mohanty <rmohanty@mail.sfsu.edu>
2025-06-26 18:59:17 -0700

    Added relevant comments.

--- cpp/header_file/om_vector.h
+++ cpp/header_file/om_vector.h
@@ -1,22 +1,30 @@
-#ifndef OM_VECTOR_H
-#define OM_VECTOR_H
+// Include Guard
+#ifndef OM_VECTOR_H  // If OM_VECTOR_H isn't defined:
+#define OM_VECTOR_H  // Defines the macro OM_VECTOR_H.
 
 #include <iostream>
 #include <vector>
 
+// Ensures that the public functions belong to "omlib" rather than the global scope.
 namespace omlib
 {
-	template<typename T>
+	template<typename T>   // Ensures that OmVector and its methods work universally.
 	class OmVector
 	{
+		// Private makes sure that declared statements 
+		// cannot be accessed outside of OmVector
 		private:
 			std::vector<T> data_vec;
 		public:
+			// Forwards to std::vector's internal push_back function.
+			// If enough mem: adds value directly. Else: allocates, copies
+			// and then adds value.
 			void push_back(const T& value)
 			{
 				data_vec.push_back(value);
 			}
-
+			// Prints the contents of data_vec. Adds commas up to n-2, then
+			// ends with n-1 and ']'.
 			void print() const
 			{
 				std::cout << "[";
@@ -26,17 +34,21 @@ namespace omlib
 				}
 				std::cout << data_vec[data_vec.size() - 1] << "]" << std::endl;
 			}
-
+			// Returns "true" if empty and "false" if data_vec 
+			// has a non-zero number of entries.
+			// const ensures that data_vec remains read-only.
 			bool empty() const
 			{
 				return data_vec.empty();
 			}
-
+			// Returns size_t containing data_vec's size.
+			// size_t chosen because guaranteed to be non-negative.
+			// Range of size_t will change with each device.
 			std::size_t size() const
 			{
 				return data_vec.size();
 			}
-	};
+	};      // End of class OmVector.
 }
 
-#endif
+#endif               // Ends conditional started by #ifndef.
