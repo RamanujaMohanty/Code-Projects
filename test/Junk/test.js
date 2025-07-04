4fdd87a7be2dd7ace96c76f75be10d4516a02f55
Ramanuja Mohanty <rmohanty@mail.sfsu.edu>
2025-07-03 22:08:17 -0700

    Delete test directory

--- test/Junk/test.js
+++
@@ -1,9 +0,0 @@
-om@DomusViridi:~/prog/html/test/junk$ cat test.js
-/*
-for (let ii = 0; ii < 5; ii++)
-{
-        console.log("Hello World!");
-}
-*/
-let x = 100;
-console.log(x.toString());
--- test/Problems/prob1.js
+++
@@ -1,9 +0,0 @@
-function addKeyValue(obj, key, value)
-{
-        obj[key] = value;
-        return obj;
-}
-const obj = {name: "Adam"};
-const newObj = addKeyValue(obj, "age", 25);
-console.log(obj);
-console.log(newObj);
