63b29291385bb16850736f6ea2aaf8ac697d68e1
RamanujaMohanty <rmohanty@mail.sfsu.edu>
2025-07-04 14:23:07 -0700

    Added struct_test.cpp to repo.

---
+++ cpp/misc-code/struct_test.cpp
@@ -0,0 +1,24 @@
+#include <fstream>
+#include <iostream>
+#include <vector>
+#include <map>
+#include <string>
+using namespace std;
+
+struct DictEntry {
+    string noun;
+    string part_of_speech;
+    string definition;
+};
+
+void open_file_check(ifstream& inFile, string& newFile) {
+
+}
+
+vector<DictEntry> newDict;
+
+
+int main()
+{
+    return 0;
+}
