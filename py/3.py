778bf16a23425aba82947b6dff3d26923af28e49
Ramanuja Mohanty <rmohanty@mail.sfsu.edu>
2025-07-07 13:17:26 -0700

    Moved files out of personal/ and into Code-Projects/py/

---
+++ py/3.py
@@ -0,0 +1,12 @@
+for n in range(19):
+	if n % 2 == 0:
+		print(n)
+print("--------------------------------")
+for a in range(18+1):
+	print(a ** 2)
+print("--------------------------------")
+for b in range(0,18+1,2):
+	print(b*2)
+print("--------------------------------")
+for d in range(10):
+	print(d+d)
---
+++ py/greet.py
@@ -0,0 +1,5 @@
+def greet_person(name):
+	print(f"Hello, {name}!")
+
+name_one = "Ronald"
+greet_person(name_one)
