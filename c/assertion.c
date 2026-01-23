9ca18211b513e0a436d6bbf46208830948201ea4
Ramanuja Mohanty <rmohanty@mail.sfsu.edu>
2026-01-22 22:40:44 -0800

    C course example files.

---
+++ c/assertion.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <assert.h>
+
+int main()
+{
+    assert(0);
+    printf("My program runs!\n");
+    return 0;
+}
---
+++ c/debug_assert.c
@@ -0,0 +1,11 @@
+#define NDEBUG
+#include <stdio.h>
+#include <ctype.h>
+#include <assert.h>
+
+int main()
+{
+    assert(0);
+    printf("My program runs!\n");
+    return 0;
+}
