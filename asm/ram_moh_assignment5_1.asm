30ec564941ba024bebd0ff7e61d4767938e755d1
RamanujaMohanty <rmohanty@mail.sfsu.edu>
2026-03-03 15:16:20 -0800

    Added assignment 5.1 file.

---
+++ asm/ram_moh_assignment5_1.asm
@@ -0,0 +1,26 @@
+
+# Description: 	User enters x. Program subtracts 5 from x repeatedly,
+#		until x <= 0, and prints x.
+#
+# #include <iostream.h>
+# int main(void)
+# {
+#   int x;
+#   
+#   cin >> x; // read an int, store in x
+#   while (x > 0)
+#     x = x - 5;
+#   
+#   cout << x << endl;
+#   
+# }
+
+# x	$s0
+
+	.data
+endl:	.asciiz	"\n"
+
+	
+	.text
+main:	
+	
