840a0c39f703384f3757c4fbaa5f09282c8b3e68
RamanujaMohanty <rmohanty@mail.sfsu.edu>
2026-03-04 20:01:26 -0800

    Finished Assignment 5.1

--- asm/assignment/ram_moh_assignment5_1.asm
+++ asm/assignment/ram_moh_assignment5_1.asm
@@ -1,4 +1,3 @@
-
 # Description: 	User enters x. Program subtracts 5 from x repeatedly,
 #		until x <= 0, and prints x.
 #
@@ -6,21 +5,43 @@
 # int main(void)
 # {
 #   int x;
-#   
+#
 #   cin >> x; // read an int, store in x
 #   while (x > 0)
 #     x = x - 5;
-#   
+#
 #   cout << x << endl;
-#   
+#
 # }
-
 # x	$s0
 
-	.data
-endl:	.asciiz	"\n"
+# Ramanuja Mohanty
+# 03/04/26
+# Assignment 5.1
 
-	
-	.text
-main:	
-	
+.data
+endl:   .asciiz	"\n"
+
+.text
+
+main:
+    li $v0, 5
+    syscall
+    move $s0, $v0
+
+loop:
+    blez $s0, end_loop
+    addi $s0, $s0, -5
+    j loop
+
+end_loop:
+    li $v0, 1
+    move $a0, $s0
+    syscall
+
+    li $v0, 4
+    la $a0, endl
+    syscall
+
+    li $v0, 10
+    syscall
